9c522bfbab3390aa4e3f12c2af8ac290d2a0f3f8
Tuana <ilknrelf@gmail.com>
2025-05-17 17:51:40 +0300

    Bug fixes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';  
-
+import 'home_screen.dart';
 import 'models/user_provider.dart'; // Make sure this path is correct
- import 'home_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserProvider(),
